2d297ef86cbbc15ae9fdd70cca7cf7916c4fa76b
pblazyca <pblazyca@outlook.com>
2023-03-04 19:58:29 +0100

    Implemented base of sending prompt to chat gpt.

--- Assets/Scripts/Assistant/UnityChatGPTAssistant.cs
+++ Assets/Scripts/Assistant/UnityChatGPTAssistant.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using OpenAI;
+using OpenAI.Chat;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -6,6 +9,8 @@ public class UnityChatGPTAssistant : EditorWindow
 {
     [field: SerializeField]
     private VisualTreeAsset VisualTreeAsset { get; set; } = default;
+
+    private OpenAIClient OpenAI { get; set; }
 
     [MenuItem("Tools/Chat GPT Assistant")]
     public static void ShowAssistant()
@@ -18,5 +23,46 @@ public class UnityChatGPTAssistant : EditorWindow
     {
         VisualElement root = VisualTreeAsset.Instantiate();
         rootVisualElement.Add(root);
+
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        string configFilePath = $"{Application.dataPath}";
+        OpenAI = new(OpenAIAuthentication.LoadFromDirectory(configFilePath));
+
+        rootVisualElement.Q<Button>("SendButton").clicked += SendPrompt;
+    }
+
+    private async void SendPrompt()
+    {
+        string prompt = rootVisualElement.Q<TextField>("PromptInput").value;
+        ScrollView chatView = rootVisualElement.Q<ScrollView>("ChatView");
+
+        List<ChatPrompt> chatPrompts = new()
+        {
+            new ("system", "You are a helpful assistant."),
+            new ("user", prompt)
+        };
+
+        ChatRequest chatRequest = new(chatPrompts);
+
+        Label userPrompt = new();
+        userPrompt.text = prompt;
+        userPrompt.style.alignSelf = Align.FlexEnd;
+        chatView.Add(userPrompt);
+
+        ChatResponse result = await OpenAI.ChatEndpoint.GetCompletionAsync(chatRequest);
+
+        Label chatResponse = new();
+        chatResponse.text = result.FirstChoice.ToString();
+        chatResponse.style.alignSelf = Align.FlexStart;
+        chatView.Add(chatResponse);
+
+        Label tokenUsage = new();
+        tokenUsage.text = $"Prompt tokens: {result.Usage.PromptTokens}, Completion tokens: {result.Usage.CompletionTokens}, Total tokens: {result.Usage.TotalTokens}";
+        tokenUsage.style.alignSelf = Align.FlexStart;
+        chatView.Add(tokenUsage);
     }
 }
