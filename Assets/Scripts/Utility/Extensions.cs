e489e3c800b3580f3bf7fd77a542c3390ecbd2fc
pblazyca <pblazyca@outlook.com>
2023-03-22 23:41:32 +0100

    Added Extension class

---
+++ Assets/Scripts/Utility/Extensions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions
+{
+    public static string LastPartOfTypeName(this Type type) {
+        string[] typeNameFull = type.FullName?.Split('.');
+        string typeNameLast = typeNameFull?[typeNameFull.Length - 1];
+        return typeNameLast;
+    }
+}
