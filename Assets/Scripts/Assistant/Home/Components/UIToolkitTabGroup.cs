be00b793722422f028b6f2ade5fa8b497d275730
pblazyca <pblazyca@outlook.com>
2023-04-12 23:49:04 +0200

    Added UIToolkitTabGroup to control group of tabs

---
+++ Assets/Scripts/Assistant/Home/Components/UIToolkitTabGroup.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace InditeHappiness.LLM.Assistant
+{
+    public class UIToolkitTabGroup
+    {
+        private UIToolkitTab CurrentTab { get; set; }
+        private List<UIToolkitTab> TabCollection { get; set; } = new();
+
+        public UIToolkitTabGroup(List<UIToolkitTab> tabs)
+        {
+            TabCollection = tabs;
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            foreach (UIToolkitTab tab in TabCollection)
+            {
+                tab.OnSelect += (UIToolkitTab selectedTab) =>
+                {
+                    if (CurrentTab != null)
+                    {
+                        CurrentTab.Deselect();
+                    }
+
+                    CurrentTab = selectedTab;
+                };
+            }
+        }
+    }
+}
